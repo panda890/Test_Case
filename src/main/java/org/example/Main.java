3047d79ecbfdb9a06b45c50080b08b27d0f6b35a
Abhishek <abhishekpanda315@gmail.com>
2023-03-24 13:06:48 +0530

    commit new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+    String s= "sam";
+        System.out.println(s);
         System.out.println("Hello world!");
     }
 }
